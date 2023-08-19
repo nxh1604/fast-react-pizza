09aca2ba1a70f3b40cf9145dbd4d0edf710bf48b
GoldenAzusa <101515269+nxh1604@users.noreply.github.com>
2023-08-19 17:29:29 +0700

    rewrite routes with createRouteElement

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,61 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./ui/Home/Home";
-import Menu, { loader as MenuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
-import CreateOrder, {
-  action as OrderAction,
-} from "./features/order/CreateOrder";
+import { Suspense, lazy } from "react";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const Home = lazy(() => import("./ui/Home/Home"));
+const Menu = lazy(() => import("./features/menu/Menu"));
+const Cart = lazy(() => import("./features/cart/Cart"));
+const CreateOrder = lazy(() => import("./features/order/CreateOrder"));
+const OrderLoading = lazy(() => import("./features/order/Order"));
+
+import { loader as MenuLoader } from "./features/menu/Menu";
+import { action as OrderAction } from "./features/order/CreateOrder";
 import { loader as OrderLoader } from "./features/order/OrderLoader";
-import OrderLoading from "./features/order/Order";
-import AppLayout from "./ui/AppLayout/AppLayout";
-import Error from "./ui/Error";
 import { action as UpdateOrderAction } from "./features/order/UpdateOrder";
 
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    errorElement: <Error />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/menu",
-        element: <Menu />,
-        errorElement: <Error />,
-        loader: MenuLoader,
-      },
-      {
-        path: "/cart",
-        element: <Cart />,
-      },
-      {
-        path: "/order/new",
-        element: <CreateOrder />,
-        action: OrderAction,
-      },
-      {
-        path: "/order/:orderId",
-        element: <OrderLoading />,
-        loader: OrderLoader,
-        errorElement: <Error />,
-        action: UpdateOrderAction,
-      },
-    ],
-  },
-]);
+import Loading from "./ui/Loading";
+import Error from "./ui/Error";
+import AppLayout from "./ui/AppLayout/AppLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements([
+    <>
+      <Route path="/" element={<AppLayout />} errorElement={<Error />}>
+        <Route index element={<Home />} />
+        <Route
+          path="menu"
+          element={<Menu />}
+          errorElement={<Error />}
+          loader={MenuLoader}
+        />
+        <Route path="cart" element={<Cart />} />
+        <Route
+          path="order/new"
+          element={<CreateOrder />}
+          action={OrderAction}
+        />
+        <Route
+          path="order/:orderId"
+          element={<OrderLoading />}
+          loader={OrderLoader}
+          errorElement={<Error />}
+          action={UpdateOrderAction}
+        />
+      </Route>
+    </>,
+  ])
+);
 
 const App = (): JSX.Element => {
-  return <RouterProvider router={router} />;
+  return (
+    <Suspense fallback={<Loading />}>
+      <RouterProvider router={router} />;
+    </Suspense>
+  );
 };
 
 export default App;
